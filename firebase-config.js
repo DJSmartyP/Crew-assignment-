1246b5cac7b49e5c06011ca1097b0cb195e229d5
DJSmartyP <nickpatel.trainer@gmail.com>
2026-08-31 18:14:28 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Paste the Firebase web-app configuration from Firebase Console here.
+// Firebase web configuration is designed to be public. Do NOT put passwords here.
+export const firebaseConfig = {
+  apiKey: "PASTE_API_KEY_HERE",
+  authDomain: "PASTE_PROJECT_ID.firebaseapp.com",
+  projectId: "PASTE_PROJECT_ID",
+  storageBucket: "PASTE_STORAGE_BUCKET_HERE",
+  messagingSenderId: "PASTE_MESSAGING_SENDER_ID",
+  appId: "PASTE_APP_ID"
+};
+
+// Copy the UID of the single Firebase Authentication account you create for GM/admin.
+// This is not the password. Firestore Security Rules enforce admin rights.
+export const ADMIN_UID = "PASTE_ADMIN_UID_HERE";
